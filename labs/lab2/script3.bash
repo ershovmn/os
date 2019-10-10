d1056a4ce45c15a7635c6886575e63913702af72
ershovmn <43749705+ershovmn@users.noreply.github.com>
2019-10-10 09:20:46 +0300

    edit script3 lab 2

--- labs/lab2/script3.bash
+++ labs/lab2/script3.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E -r -h -s -o -w "[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+.[a-z]+" /etc/ | awk '{print $1}' > ./emails.lst
+grep -E -r -h -s -w -o "[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+.[a-z]+" /etc/ | awk '{print $1}' > ./emails.lst
