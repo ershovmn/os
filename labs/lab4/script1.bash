df968435ffc69f0330bc8c1cef36fd49883c0a0f
ershovmn <43749705+ershovmn@users.noreply.github.com>
2019-10-12 14:47:24 +0300

    edit lab4 1

--- labs/lab4/script1.bash
+++ labs/lab4/script1.bash
@@ -2,7 +2,7 @@
 d=$(date +"%m_%d_%y_%H_%M_%S")
 mkdir "$HOME/test" &&
 {
-    echo "catalog test was created successfully" > "./report"
-    touch "$HOME/test/$d"
+        echo "catalog test was created successfully" > "/home/tehnarenok/labs/lab4/report"
+        touch "$HOME/test/$d"
 }
-ping -c 1 www.net_nikogo.ru || echo "ERROR connection" >> "./report"
+ping -c 1 www.net_nikogo.ru || echo "ERROR connection" >> "/home/tehnarenok/labs/lab4/report"
