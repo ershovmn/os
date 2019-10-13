4a11f916d0aa4282e2492ecfc0c39be13c061deb
ershovmn <43749705+ershovmn@users.noreply.github.com>
2019-10-13 10:57:33 +0300

    edit lab4 script3

--- labs/lab4/script3.bash
+++ labs/lab4/script3.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 crontab -l > mycron
-echo "*/5 * * * 3 /home/tehnarenok/labs/lab4/script1" >> mycron
+echo "*/5 * * * 3 /home/tehnarenok/labs/lab4/script1.bash" >> mycron
 crontab mycron
 rm mycron
